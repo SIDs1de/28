5250181246735c749dcccc6def684911be981535
SIDs1de <82277183+SIDs1de@users.noreply.github.com>
2023-01-11 19:19:33 +0300

    Add files via upload

--- libs/site.js
+++ libs/site.js
@@ -330,18 +330,11 @@ $('.js-feedback2').magnificPopup({
   }
 });
 
-alert(12)
-
 if (document.querySelector('body').classList.contains('protection')) {
   const body = document.querySelector('body');
   window.addEventListener('scroll',  () => {
-    alert(1);
     const btns = document.querySelectorAll('.showcase__block-btns .showcase__btn');
-    btns[0].innerHTML = window.pageYOffset;
-    btns[1].innerHTML = document.body.scrollHeight - document.documentElement.clientHeight;
-    console.log(window.pageYOffset);
-    console.log(document.body.scrollHeight - document.documentElement.clientHeight);
-    if (window.pageYOffset + document.documentElement.clientHeight >= document.body.scrollHeight) {
+    if (window.pageYOffset + document.documentElement.clientHeight + 60 >= document.body.scrollHeight) {
       if (!document.querySelector('.showcase__btn--active')) {
         if (!body.classList.contains('blocked')) {
           body.classList.add('blocked')
